150f1ba261be2ea51a1418e32d00d1d45b9b8a66
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-05-08 00:01:51 -0500

    Added linux support to gitbatch

--- gitbatch.rb
+++ gitbatch.rb
@@ -5,6 +5,11 @@ class Gitbatch < Formula
   version "0.4.3"
   sha256 "29650ab55ec2af713f7a1d9a5953aae3f45a79fb21ece9513592241def91d77a"
 
+  if OS.linux? && Hardware::CPU.is_64_bit?
+    url "https://github.com/isacikgoz/gitbatch/releases/download/v0.4.3/gitbatch_0.4.3_linux_amd64.tar.gz"
+    sha256 "cadd377e5672281c62b304768110c1a0b36ed752e133d52ae220dedb6d56db69"
+  end
+
   def install
     bin.install "gitbatch"
   end
