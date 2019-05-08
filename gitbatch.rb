517bd8471b3df8e6e420df6869c46a9d048940a0
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-05-07 23:57:50 -0500

    Updated gitbatch to 0.4.3

--- gitbatch.rb
+++ gitbatch.rb
@@ -1,9 +1,9 @@
 class Gitbatch < Formula
   desc "manage your git repositories in one place"
   homepage "https://github.com/isacikgoz/gitbatch"
-  url "https://github.com/isacikgoz/gitbatch/releases/download/v0.4.2/gitbatch_0.4.2_darwin_amd64.tar.gz"
-  version "0.4.2"
-  sha256 "b76624da777c10aa43c835f31355c9ae96162e27b16b6c1d92ed4a83275b6f29"
+  url "https://github.com/isacikgoz/gitbatch/releases/download/v0.4.3/gitbatch_0.4.3_darwin_amd64.tar.gz"
+  version "0.4.3"
+  sha256 "29650ab55ec2af713f7a1d9a5953aae3f45a79fb21ece9513592241def91d77a"
 
   def install
     bin.install "gitbatch"
