d582175e01ca8a8815a5769fd8e35486080765e8
Jaden Pleasants <jadenpleasants@fastmail.com>
2019-05-08 00:09:00 -0500

    Added linux support to tldr

--- tldr.rb
+++ tldr.rb
@@ -5,6 +5,11 @@ class Tldr < Formula
   version "0.5.0"
   sha256 "809eaa40eae97db8564a4c864bc1be085f62c3ed08ec5c1bee3f43113202a636"
 
+  if OS.linux? && Hardware::CPU.is_64_bit?
+    url "https://github.com/isacikgoz/tldr/releases/download/v0.5.0/tldr_0.5.0_linux_amd64.tar.gz"
+    sha256 "036f0399cfe17ce117873b6d06484d125200c572914bc2860512a2659e170641"
+  end
+
   def install
     bin.install "tldr"
   end
