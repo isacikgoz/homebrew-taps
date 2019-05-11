9eaf6cb6f97b19ae963ea5bb1f7a68b52ec263e0
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-05-11 14:39:28 +0300

    update tldr version

--- tldr.rb
+++ tldr.rb
@@ -1,13 +1,13 @@
 class Tldr < Formula
   desc "fast and interactive tldr client written with go"
   homepage "https://github.com/isacikgoz/tldr"
-  url "https://github.com/isacikgoz/tldr/releases/download/v0.5.0/tldr_0.5.0_darwin_amd64.tar.gz"
-  version "0.5.0"
-  sha256 "809eaa40eae97db8564a4c864bc1be085f62c3ed08ec5c1bee3f43113202a636"
+  url "https://github.com/isacikgoz/tldr/releases/download/v0.6.1/tldr_0.6.1_darwin_amd64.tar.gz"
+  version "0.6.1"
+  sha256 "71ebce6d81a4c009868af0c3a9cc710b4dcce6184593072ff4c31ae955001404"
 
   if OS.linux? && Hardware::CPU.is_64_bit?
-    url "https://github.com/isacikgoz/tldr/releases/download/v0.5.0/tldr_0.5.0_linux_amd64.tar.gz"
-    sha256 "036f0399cfe17ce117873b6d06484d125200c572914bc2860512a2659e170641"
+    url "https://github.com/isacikgoz/tldr/releases/download/v0.6.1/tldr_0.6.1_linux_amd64.tar.gz"
+    sha256 "2c3652877f9792956e7d4d3e6d85305e347326b15abcae68c96a7e6e195977b6"
   end
 
   def install
@@ -15,6 +15,6 @@ class Tldr < Formula
   end
 
   test do
-    assert_match "tldr version 0.5.0", shell_output("#{bin}/tldr --version", 2)
+    assert_match "tldr version 0.6.1", shell_output("#{bin}/tldr --version", 2)
   end
 end
