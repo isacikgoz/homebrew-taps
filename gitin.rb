531cf2fa6e814017652e1383fe3301989155dad7
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-05-12 13:10:49 +0300

    add gitin to linuxbrew

--- gitin.rb
+++ gitin.rb
@@ -6,6 +6,11 @@ class Gitin < Formula
   sha256 "111cf7e178fcfe8dc6fdee0766458b0c8c4de345eb87aa9b6ca982fdfc5ac798"
   depends_on "libgit2"
 
+  if OS.linux? && Hardware::CPU.is_64_bit?
+    url "https://github.com/isacikgoz/gitin/releases/download/v0.1.5/gitin_0.1.5_linux_amd64.tar.gz"
+    sha256 "3b29e931972d4a104d41ccc49e74e62eb38f4c50c78cc8f660a702626ca372bc"
+  end
+
   def install
     bin.install "gitin"
   end
