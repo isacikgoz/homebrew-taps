80548b6edd0515b595f1c030b4c4de913e1abf11
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-06-19 09:07:54 +0300

    update gitbatch to 0.5.0

--- gitbatch.rb
+++ gitbatch.rb
@@ -1,12 +1,12 @@
 class Gitbatch < Formula
   desc "manage your git repositories in one place"
   homepage "https://github.com/isacikgoz/gitbatch"
-  url "https://github.com/isacikgoz/gitbatch/releases/download/v0.4.3/gitbatch_0.4.3_darwin_amd64.tar.gz"
-  version "0.4.3"
-  sha256 "29650ab55ec2af713f7a1d9a5953aae3f45a79fb21ece9513592241def91d77a"
+  url "https://github.com/isacikgoz/gitbatch/releases/download/v0.5.0/gitbatch_0.5.0_darwin_amd64.tar.gz"
+  version "0.5.0"
+  sha256 "adc2bf76ea03af61aeb80cf4ba314c06b67327d1e76a9e501727ae7e763e1f3a"
 
   if OS.linux? && Hardware::CPU.is_64_bit?
-    url "https://github.com/isacikgoz/gitbatch/releases/download/v0.4.3/gitbatch_0.4.3_linux_amd64.tar.gz"
+    url "https://github.com/isacikgoz/gitbatch/releases/download/v0.5.0/gitbatch_0.5.0_linux_amd64.tar.gz"
     sha256 "cadd377e5672281c62b304768110c1a0b36ed752e133d52ae220dedb6d56db69"
   end
 
@@ -15,6 +15,6 @@ class Gitbatch < Formula
   end
 
   test do
-    assert_match "gitbatch version 0.4.2", shell_output("#{bin}/gitbatch --version", 2)
+    assert_match "gitbatch version 0.5.0", shell_output("#{bin}/gitbatch --version", 2)
   end
 end
