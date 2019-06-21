ec418694c7060e950c614b365a537b26b2e34a52
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2019-06-21 11:16:16 +0300

    update gitin version to 0.2.1

--- gitin.rb
+++ gitin.rb
@@ -1,14 +1,14 @@
 class Gitin < Formula
   desc "commit/branch/workdir explorer for git"
   homepage "https://github.com/isacikgoz/gitin"
-  url "https://github.com/isacikgoz/gitin/releases/download/v0.2.0/gitin_0.2.0_darwin_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "576361e3553fdd82569df145f807c0be0c09072250aeb900b4a2c5474752ed3b"
+  url "https://github.com/isacikgoz/gitin/releases/download/v0.2.1/gitin_0.2.1_darwin_amd64.tar.gz"
+  version "0.2.1"
+  sha256 "772a96a4aecf8e2553d91f6f42e92e63e2293490b7c6e8e398d91cc9dff5ad47"
   depends_on "libgit2"
 
   if OS.linux? && Hardware::CPU.is_64_bit?
-    url "https://github.com/isacikgoz/gitin/releases/download/v0.2.0/gitin_0.2.0_linux_amd64.tar.gz"
-    sha256 "65c413e3417c60f8d1d3418a1f8b59141122eb882b031633ac6d141b15ce23b5"
+    url "https://github.com/isacikgoz/gitin/releases/download/v0.2.1/gitin_0.2.1_linux_amd64.tar.gz"
+    sha256 "a11cddbcc20a91c1e29e902f3152158bd2f6d60069d220a7adac7fff5fb1be7f"
   end
 
   def install
@@ -16,6 +16,6 @@ class Gitin < Formula
   end
 
   test do
-    assert_match "gitin version 0.2.0", shell_output("#{bin}/gitin --version", 2)
+    assert_match "gitin version 0.2.1", shell_output("#{bin}/gitin --version", 2)
   end
 end
