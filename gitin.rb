f7f26a695fdeacbb095727ce34ea361af20e0574
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-09-28 11:41:04 +0300

    update gitin

--- gitin.rb
+++ gitin.rb
@@ -1,14 +1,14 @@
 class Gitin < Formula
   desc "commit/branch/workdir explorer for git"
   homepage "https://github.com/isacikgoz/gitin"
-  url "https://github.com/isacikgoz/gitin/releases/download/v0.2.1/gitin_0.2.1_darwin_amd64.tar.gz"
-  version "0.2.1"
-  sha256 "772a96a4aecf8e2553d91f6f42e92e63e2293490b7c6e8e398d91cc9dff5ad47"
+  url "https://github.com/isacikgoz/gitin/releases/download/v0.2.5/gitin_v0.2.5_darwin_amd64.tar.gz"
+  version "0.2.5"
+  sha256 "fa22f8df894a81393b19f6369432c39bac65cace88d2d9dd252a96ba35d08391"
   depends_on "libgit2"
 
   if OS.linux? && Hardware::CPU.is_64_bit?
-    url "https://github.com/isacikgoz/gitin/releases/download/v0.2.1/gitin_0.2.1_linux_amd64.tar.gz"
-    sha256 "a11cddbcc20a91c1e29e902f3152158bd2f6d60069d220a7adac7fff5fb1be7f"
+    url "https://github.com/isacikgoz/gitin/releases/download/v0.2.5/gitin_v0.2.5_linux_amd64.tar.gz"
+    sha256 "e768667eee5e51d8a30e53b45d4fa56f87109251b7afed15da326c91200ca417"
   end
 
   def install
